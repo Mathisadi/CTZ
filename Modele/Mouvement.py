fd9f976f4d2a181d97a0e7da5bb7d9b618572fa6
Mathisadi <adinolfimathis@gmail.com>
2024-12-12 14:33:02 +0100

    Reformat et mouvment pieton

--- Modele/Mouvement.py
+++ Modele/Mouvement.py
@@ -10,51 +10,75 @@ from Modele import *
 # Objectif définir une fonction qui déplace les voitures et met à jour le trafic
 
 def mouvement_pieton(route, trafic):
+    """
+    Fonction qui permet de gérer le mouvement des piétons sur la route.
+    Les piétons sont générés sur les cases de départ piétons et se déplacent sur les cases de passage piétons.
+    Les piétons sont supprimés de la route quand ils finissent de traverser.
+    
+    Parameters:
+    route (2D list): La route sur laquelle on simule le mouvement des piétons.
+    trafic (2D list): Le trafic actuel de la route.
+    
+    Returns:
+    route (2D list): La route mise à jour.
+    trafic (2D list): Le trafic mis à jour.
+    """
         
-    # Dans un premier temps on bouge les piétons aux abords de la route
+    # Dans un premier temps : on bouge les piétons aux abords de la route
     # On teste tous les éléments
     for x in range(len(route)):
         for y in range(len(route[x])):
             if route[x][y] != 0:
                 if route[x][y][0] == "Depart_pieton":
                     if route[x][y][4] and trafic[x][y][0] == 1:
+                        # On se déplace sur le passage piéton adjacent
                         dx, dy = repere[route[x][y][1]]
                         nx, ny = x + dx, y + dy
+                        
+                        # On met à jour le passage piéton
                         route[nx][ny][2] = 4
                         route[nx][ny][3] = True
+                        
+                        # On enlève le piéton du trottoir
                         trafic[x][y][0] = 0
     
-    # Dans un second temps
+    # Dans un second temps : on bouge les piétons sur les passages piétons
+    # On teste tous les éléments
+    for x in range(len(route)):
+        for y in range(len(route[x])):
+            if route[x][y] != 0:
+                if route[x][y][0] == "Pieton":
+                    if route[x][y][2] == 0 and route[x][y][3]:
+                        # On se déplace sur le bloc adjacent
+                        dx, dy = repere[route[x][y][1]]
+                        nx, ny = x + dx, y + dy
                         
-                        
-def mouvement(route, direction, trafic, temps):
-    """Cette fonction a pour but de faire avancer les voitures et de mettre à jour le trafic,
-    1 itération correspond à 1 seconde.
+                        # Si c'est un passage piéton on le met à jour
+                        if route[nx][ny][0] == "Pieton":
+                            route[nx][ny][2] = 4
+                            route[nx][ny][3] = True
+                            
+                        # On enléve le piéton du passage
+                        route[x][y][3] = False
+    
+    return route, trafic
 
-    Args:
-        route (2D list): Liste des élements de notre route
-        direction (2D list): Liste des préferences de direction des utilisateur pour chaque élement et la direction choisie
-        trafic (2D list): Liste resprésentant le trafic de notre route 0 = vide / 1 = voiture
-        temps (int): Temps depuis début de la simulation en seconde
-
-    Returns:
-        route (2D list): Element de la route mis à jour à t = temps
-        trafic (2D list): trafic mis à jour aprés mouvement des voitures
-        direction (2D list): Mise à jour des directions dans les intersections
+def mouvement_entre_blocs(route, direction, trafic, ref_trafic):
     """
-
-    # Update des éléments
-    route, direction, trafic = update_grille(route, direction, trafic, temps)
+    Fonction qui permet de gérer le mouvement des voitures entre les blocs.
+    Les voitures sont déplacées si le bloc suivant est vide, sinon elles ne bougent pas.
     
-    # On garde en copie le trafic
-    ref_trafic = copy.deepcopy(trafic)
+    Parameters:
+    route (2D list): La route sur laquelle on simule le mouvement des voitures.
+    direction (2D list): Les directions préférées des utilisateurs pour chaque élément de la route.
+    trafic (2D list): Le trafic actuel de la route.
+    ref_trafic (2D list): Le trafic actuel de la route au tour précédent.
     
-    # Mouvement des piétons
-    
-    
-    # Mouvement entre les blocs
-    
-    # Mouvement dans les blocs
+    Returns:
+    route (2D list): La route mise à jour.
+    direction (2D list): Les directions mises à jour.
+    trafic (2D list): Le trafic mis à jour.
+    """
     
     # Dans un premier temps on fait déplacer les voitures entre les blocs
     deplacement_entre_bloc = changement_bloc(route, direction, trafic)
@@ -78,7 +102,7 @@ def mouvement(route, direction, trafic, temps):
                 trafic[x_dest][y_dest][0] += 1
                 trafic[x][y][-1] -= 1
 
-            # Cas particulier non/intersection->intersection
+        # Cas particulier non/intersection->intersection
         elif route[x_dest][y_dest][0] == "Intersection" and route[x][y][0] != "Intersection":
             if trafic[x_dest][y_dest][0] == 0 and ref_trafic[x_dest][y_dest][0] == 0:
                 direction[x_dest][y_dest] = chemin_intersection(route, direction, (x, y))
@@ -103,9 +127,25 @@ def mouvement(route, direction, trafic, temps):
                 trafic[x][y][-1] -= 1
             elif trafic[x_dest][y_dest][0] == 0 and ref_trafic[x_dest][y_dest][0] == 0:
                 trafic[x_dest][y_dest][0] += 1
-                trafic[x][y][-1] -= 1                
+                trafic[x][y][-1] -= 1           
+                
+    return route, direction, trafic
 
-    # Dans un second temps on fait déplacer les voitures à l'intérieur des blocs seul les routes sont concernées
+def mouvement_route(route, trafic, ref_trafic):
+    """
+    Cette fonction permet de gérer le mouvement des voitures dans les routes.
+    
+    Args:
+        route (2D list): La route actuelle.
+        trafic (2D list): Le trafic actuel de la route.
+        ref_trafic (2D list): Le trafic actuel de la route au tour précédent.
+    
+    Returns:
+        route (2D list): La route mise à jour.
+        trafic (2D list): Le trafic mis à jour.
+    """
+    
+    # On trouve la taille de la grille
     n, m = len(route), len(route[0])
     
     # On test tous les bloc de la grille
@@ -121,5 +161,37 @@ def mouvement(route, direction, trafic, temps):
                         ):
                             trafic[x][y][-k + 1] += 1
                             trafic[x][y][-k] -= 1
+                            
+    return route, trafic
+                            
+def mouvement(route, direction, trafic, temps):
+    """
+    Met à jour la route, les directions et le trafic en fonction des conditions actuelles des routes, des directions,
+    et du trafic pour un instant donné.
 
+    Args:
+        route (2D list): Liste des éléments de la route, représentant l'état de chaque cellule.
+        direction (2D list): Liste des préférences de direction pour chaque élément de la route.
+        trafic (2D list): Représentation du trafic sur la route, indiquant la présence de voitures et piétons.
+        temps (int): Temps écoulé depuis le début de la simulation en secondes.
+
+    Returns:
+        tuple: Retourne la route mise à jour, les directions mises à jour, et le trafic mis à jour après le traitement des piétons, des voitures, et des feux rouges.
+    """
+    
+    # Update des éléments   
+    route, direction, trafic = update_grille(route, direction, trafic, temps)
+    
+    # On garde en copie le trafic
+    ref_trafic = copy.deepcopy(trafic)
+    
+    # Mouvement des piétons
+    route, trafic = mouvement_pieton(route, trafic)
+    
+    # Mouvement entre les blocs
+    route, direction, trafic = mouvement_entre_blocs(route, direction, trafic, ref_trafic)
+    
+    # Mouvement dans les blocs
+    route, trafic = mouvement_route(route, trafic, ref_trafic)
+    
     return route, direction, trafic
