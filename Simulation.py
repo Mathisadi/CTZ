7f087361de0a7e9f560478bee3c9431cd0418774
Mathisadi <adinolfimathis@gmail.com>
2024-12-26 14:51:05 +0100

    Organisation

--- Simulation.py
+++ Simulation.py
@@ -3,7 +3,7 @@
 
 # Bibliothéques utilisées
 from Modele import *
-from Variables import *
+from Var import *
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.animation import FuncAnimation
--- test.py
+++
@@ -1,4 +0,0 @@
-a = [1,2]
-b = [True, False, False, False]
-
-print(all([b[i] for i in a]))
