db5f9b1b9144b7972a0483ff8051730a4bd6fa89
Mathisadi <adinolfimathis@gmail.com>
2025-02-27 22:41:50 +0100

    route ok

--- Interface/src/stores/routeSettings.js
+++ Interface/src/stores/routeSettings.js
@@ -4,15 +4,17 @@ import { defineStore } from "pinia";
 export const routeSettigns = defineStore(
   "routeSettings",
   () => {
-    // Liste des parametres d'une route
-    const sens_route = ref([]);
-    const direction_possible = ref([]);
-    const longeur = ref(0);
+    // Liste des parametres d'une route*
+    let nom = ref("None");
+    let sens_route = ref([]);
+    let direction_possible = ref([]);
+    let longeur = ref(0);
 
     return {
       sens_route,
       direction_possible,
       longeur,
+      nom
     };
   },
   {
