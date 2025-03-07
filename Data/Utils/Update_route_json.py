db907444fb6824d2b2da6e5b952cbabc9e126c68
Mathisadi <adinolfimathis@gmail.com>
2025-03-07 16:32:34 +0100

    Automatisation de la création de route.json

---
+++ Data/Utils/Update_route_json.py
@@ -0,0 +1,92 @@
+import json
+
+# On crée un fonction qui crée la value de la route à partir d'un element de data
+def create_route_value(element):
+    # On récupère les infos
+    info = element["info"]
+    
+    return ["Route", info["sens"]]
+
+# On crée un fonction qui crée la value de l'intersection à partir d'un element de data
+def create_intersection_value(element):
+    # Direction possible et numéro de l'intersection
+    return ["Intersection", [True, True, True, True], 0]
+
+# On crée un fonction qui crée la value du feu à partir d'un element de data
+def create_feu_value(element):
+    # On récupère les infos
+    info = element["info"]
+
+    return ["Feu", info["sens"], info["cycle"], info["etat"]]
+
+# On crée un fonction qui crée la value de la priorité à partir d'un element de data
+def create_priorite_value(element):
+    # On récupère les infos
+    info = element["info"]
+
+    return ["Priorité", info["sens"]]
+
+# On crée un fonction qui crée la value du départ à partir d'un element de data
+def create_depart_value(element):
+    # On récupère les infos
+    info = element["info"]
+
+    if info["type"] == "depart_voiture":
+        return ["Depart", info["sens"], info["densite"]]
+    else:
+        return ["Depart_pieton", info["sens"], info["densite"], info["cycle"], info["etat"]]
+    
+# On crée une fonction qui crée la value du piéton à partir d'un element de data
+def create_pieton_value(element):
+    # On récupère les infos
+    info = element["info"]
+
+    return ["Pieton", info["sens"], [0, 0, 0, 0], [False, False, False, False]]
+
+# On crée une fonction qui crée la value de la fin à partir d'un element de data
+def create_arrivee_value(element):
+    # On récupère les infos
+    info = element["info"]
+
+    return ["Fin", info["sens"]]
+
+# On crée une fonction qui prend un élément et qui crée la value
+def create_value(element):
+    # On récupère les infos
+    info = element["info"]
+    type = info["type"]
+
+    if type == "route":
+        return create_route_value(element)
+    elif type == "intersection":
+        return create_intersection_value(element)
+    elif type == "feux":
+        return create_feu_value(element)
+    elif type == "priorité":
+        return create_priorite_value(element)
+    elif type == "depart_voiture":
+        return create_depart_value(element)
+    elif type == "piéton":
+        return create_pieton_value(element)
+    elif type == "depart_pieton":
+        return create_depart_value(element)
+    elif type == "fin":
+        return create_arrivee_value(element)
+
+# On à pour but de créer les json route trafic et direction
+def update_route_json():
+    with open("./Data/Data.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+    
+    res = {"route" :[]}
+
+    for item in data["data"]:
+        item_id = item["item_id"]
+        value = create_value(item)
+        res["route"].append({"item_id": item_id, "value": value})
+
+    with open("./Data/Route.json", "w", encoding="utf-8") as f:
+        json.dump(res, f, ensure_ascii=False, indent=4) 
+
+
+
