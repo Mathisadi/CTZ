4dfd3b4031964d5dbc78858bf4955f934d60bdfb
Mathisadi <adinolfimathis@gmail.com>
2025-03-11 15:56:05 +0100

    C'est quoi ça ?!

---
+++ Data/Utils/__init__.py
@@ -0,0 +1,12 @@
+# @Autor : Mathis Adinolfi
+# @Date of creation : 30/11/2024
+
+from .Init_variables import *
+from .Clear import *
+
+__all__ = [
+    "init_variables_json",
+    "custom_json_dumps",
+    "clear_data",
+    "clear_res"
+]
