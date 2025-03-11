116bdee649daba079ef1c319e13c5fce30a4e4c3
Mathisadi <adinolfimathis@gmail.com>
2025-03-11 15:53:55 +0100

    connect with api

--- Interface/src/stores/simulation.js
+++ Interface/src/stores/simulation.js
@@ -4,17 +4,55 @@ import { defineStore } from "pinia";
 export const simulation = defineStore(
   "simulation",
   () => {
-    const isSimulated = ref(false)
+    // Constante
+    const isSimu = ref(false);
+    const isLoading = ref(false)
 
-    const startSimulation = () => {
-      isSimulated.value = true;
-    };
+    // On appelle l'api pour savoir si on a simulé ou pas
+    async function isSimulated() {
+      // Le path
+      const path = "/api/isSimu";
+
+      try {
+        const response = await fetch(path, {
+          method: "GET",
+        });
+
+        // Réponse
+        const result = await response.json();
+        isSimu.value = result;
+        isLoading.value = false;
+
+      } catch (error) {
+        console.error("Erreur:", error);
+        isLoading.value = false;
+      }
+    }
+
+    async function launchSimulation() {
+      // Le path
+      const path = "/api/simulation/launch";
+
+      isLoading.value = true;
+
+      try {
+        const response = await fetch(path, {
+          method: "POST",
+        });
+      isSimu.value = true
+      } catch (error) {
+        console.error("Erreur:", error);
+      }
+    }
 
     return {
-      isSimulated
+      isSimulated,
+      launchSimulation,
+      isSimu,
+      isLoading
     };
   },
   {
-    persist: true,
+    persist: false,
   }
 );
